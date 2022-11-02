2bd9d30c36bd100114c1004ddbac51d4f0fab2be
helixgade <helixgade@gmail.com>
2022-11-02 22:13:07 +0100

    added the first few screens

---
+++ lib/Widgets/tags.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import '../Style/colors.dart';
+
+Widget Tag(BuildContext context,
+    {required VoidCallback onTap, required String text, Color? textColor}) {
+  return GestureDetector(
+    onTap: onTap,
+    child: Container(
+        alignment: Alignment.center,
+        padding: const EdgeInsets.all(7),
+        decoration: BoxDecoration(
+            border: Border.all(color: AppColor.blue, width: 1),
+            borderRadius: BorderRadius.circular(100)),
+        child: Text(
+          text,
+          style: TextStyle(
+              fontSize: 12,
+              color: textColor ?? AppColor.primaryColor,
+              fontFamily: ''),
+        )),
+  );
+}
